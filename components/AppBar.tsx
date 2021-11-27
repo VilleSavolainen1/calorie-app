82075df1e1acb431c40943c69e2076081b4d3963
Ville <visavola@gmail.com>
2021-11-27 20:57:07 +0200

    created navigation links

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,2 +1,30 @@
 import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Link } from 'react-router-native';
+import Constants from 'expo-constants';
 
+const AppBar = () => {
+    return (
+        <View style={styles.container}>
+            <Link to="/"><Text style={styles.link}>Home</Text></Link>
+            <Link to="/calculator"><Text style={styles.link}>Calculator</Text></Link>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: Constants.statusBarHeight,
+        backgroundColor: '#333',
+        height: 50,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        flexDirection: 'row'
+    },
+    link: {
+        color: '#fff',
+        fontSize: 22
+    }
+})
+
+export default AppBar;
